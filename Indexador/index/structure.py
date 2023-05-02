f80126ea2391e1ed634ef46df18dac5f807e3000
Victor <victorlrmatos@gmail.com>
2023-05-02 18:05:08 -0300

    atividade 9

--- Indexador/index/structure.py
+++ Indexador/index/structure.py
@@ -328,7 +328,33 @@ class FileIndex(Index):
                 term = self.next_from_file(idx_file)
 
     def get_occurrence_list(self, term: str) -> List:
-        return []
+        occurrence_list = []
+        # existe no dicionario?
+        if term in self.dic_index.keys():
+            # entao o termo existe e ele tem um id
+            term_id = self.dic_index[term].term_id
+
+            # pode occorrer mais de uma vez, por isso eh uma lista
+
+            # abrir o arquivo e ir para a posicao de inicio do termo
+            idx_file = open(self.str_idx_file_name, 'rb')
+            idx_file.seek(self.dic_index[term].term_file_start_pos)
+
+            # consumir a primeira ocorrencia
+            next = self.next_from_file(idx_file)
+            # enquanto o next tiver o mesmo term_id do passado na busca vai add
+            while (next != None and next.term_id == term_id):
+                occurrence_list.append(next)
+                # ja ta em ordem, so chamar a proxima ocorrencia
+                next = self.next_from_file(idx_file)
+
+            # ja add todos
+            idx_file.close()
+        else:
+            pass  # se nao ta no dicionario, o termo nao ocorre no arquivo
+        return occurrence_list
 
     def document_count_with_term(self, term: str) -> int:
+        if term in self.dic_index:
+            return self.dic_index[term].doc_count_with_term
         return 0
